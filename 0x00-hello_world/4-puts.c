9efa040aa97b2b14ef358a08b0edef95622a9f54
Lawrence <olaleyelawrence8@gmail.com>
2022-10-13 16:10:31 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *  *main- Entry point for the program
+ *
+ *   *
+ *
+ *    *Return: Always 0 no erros otherwise returns non-zero value.
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
