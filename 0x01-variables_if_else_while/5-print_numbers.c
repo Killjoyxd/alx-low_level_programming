58ba913f9142657e344cf60f7914220a6bdd993e
Lawrence <olaleyelawrence8@gmail.com>
2022-10-17 15:14:11 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all strings from 0-9
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0; i > 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+	return (0);
+}
