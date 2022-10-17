01ea5aa5b70d7b4f7b29d3ca86348ee5ffa1802b
Lawrence <olaleyelawrence8@gmail.com>
2022-10-17 14:33:42 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- pritnts alphabet in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	chart i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
