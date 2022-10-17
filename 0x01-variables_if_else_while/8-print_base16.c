0c39d1a07c24da68ac16e5597af102b78a7a2587
Lawrence <olaleyelawrence8@gmail.com>
2022-10-17 16:54:49 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers in base 16
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+		putchar((i % 10) + '0');
+
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+
+	putchar('\n');
+	return (0);
+}
